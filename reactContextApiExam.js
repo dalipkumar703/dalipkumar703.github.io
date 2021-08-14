75bc71e0fa842a0063857aabe5e40bf83a4d58ea
dalip <dalipkumar703@gmail.com>
2021-08-15 00:19:40 +0530

    Update reactContextApiExam.js

--- reactContextApiExam.js
+++ reactContextApiExam.js
@@ -49,3 +49,44 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+/// context Api in functional component
+import React, { useState , useContext} from 'react';
+import ReactDOM from 'react-dom';
+
+const languages = ['JavaScript', 'Python'];
+const LanguageContext = React.createContext();
+
+
+const LanguageProvider= LanguageContext.Provider;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      // implement Context here so can be used in child components
+      <LanguageProvider value={languages}>
+      <MainSection />
+      </LanguageProvider>
+    );
+  }
+}
+
+const MainSection =  ()=> {
+  const [itemSelected, setItemSelected] = useState(0);
+  const context = useContext(LanguageContext);
+  const handleOnClick= ()=>{
+    setItemSelected(!itemSelected )
+  }
+
+     return(   <div>
+        <p id="favoriteLanguage">Favorite programing language: {itemSelected? context[1]:context[0]}</p>
+        <button id="changeFavorite" onClick={handleOnClick}>Toggle language</button>
+      </div>)
+        
+    
+  
+}
+
+export default App
