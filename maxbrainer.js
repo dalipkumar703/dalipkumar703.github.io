2128d1f604744dbf76f2b00d058b7bed171db03e
dalip <dalipkumar703@gmail.com>
2024-05-11 10:20:42 +0530

    Create maxbrainer.js

---
+++ maxbrainer.js
@@ -0,0 +1,14 @@
+function maxBrainer(arr){
+let dp = new Array(arr.length+ 1).fill(0);
+let n = arr.length;
+dp[n - 1] = arr[n - 1][0]
+for (let i = n - 2; i >= 0; i++){
+let take = arr[i][0] + (i + arr[i][1]+ 1 < n ? dp[i + arr[i][1] + 1]: 0)
+let not_take = dp[i + 1];
+dp[i] = Math.max(take,not_take)
+}
+console.log(dp)
+return dp[arr.length - 1]
+
+}
+console.log(maxBrainer([[3,2],[4,3],[4,4],[2,5]]))
