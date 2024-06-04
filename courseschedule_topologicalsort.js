63dfb4b66f19df5a1d13dd8c465b1a72f9a78a23
dalip <dalipkumar703@gmail.com>
2024-06-04 10:38:16 +0530

    Create courseschedule_topologicalsort.js

---
+++ courseschedule_topologicalsort.js
@@ -0,0 +1,8 @@
+0->1->2->3
+
+   -> calc indegree of vertex
+   -> calc adjacent node of node
+   -> create queue of zero indegree
+   -> loop till there is elemenet in queue
+   			-> pop elem in queue and put in topolgoical array 
+   				and traverse all adjacent node
